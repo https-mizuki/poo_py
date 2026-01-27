2ecf7101d8d709ff1de69b4e89729e658b485db7
José Guilherme <amorimjoseguilherme@gmail.com>
2026-01-27 19:39:32 -0300

    se a nota for maior que 5 não registra e se não houver notas imprime "sem avaliações"

--- curso-python-2-3914fc83e8bc2655aee1bc409cbb8a92c6152b41/app.py
+++ curso-python-2-3914fc83e8bc2655aee1bc409cbb8a92c6152b41/app.py
@@ -1,9 +1,10 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
+restaurante_praca.receber_avaliacao('Gui', 5)
+restaurante_praca.receber_avaliacao('Lais', 10)
 restaurante_praca.receber_avaliacao('Emy', 2)
+
 
 def main():
     Restaurante.listar_restaurantes()
