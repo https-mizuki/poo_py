5c4f539eb74d63350c5f10bd60b929d285763c8c
José Guilherme <amorimjoseguilherme@gmail.com>
2026-01-30 12:28:04 -0300

    adicionando classe bebida com herança a classe ItemCardapio

---
+++ Restaurante-model/modelos/cardapio/bebida.py
@@ -0,0 +1,7 @@
+from modulo.cardapio.item_cardapio import ItemCardapio
+
+class Bebida(ItemCardapio):
+    def __init__(self,nome,preco,tamanho):
+        super().__init__(nome,preco)
+        self.tamanho= tamanho
+        
