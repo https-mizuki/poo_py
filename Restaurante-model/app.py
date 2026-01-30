77a4260714d749be928e2314af18582ca9e1e541
José Guilherme <amorimjoseguilherme@gmail.com>
2026-01-30 17:40:32 -0300

    Criando desconto e aplicando com polimorfismo

--- Restaurante-model/app.py
+++ Restaurante-model/app.py
@@ -4,13 +4,14 @@ from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida("suco de melancia",5.00, 'grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Pãozinho',2.00,'O melhor pãozinho da cidade')
-
+prato_paozinho.aplicar_desconto()
 restaurante_praca.adicionar_item_ao_cardapio(bebida_suco)
 restaurante_praca.adicionar_item_ao_cardapio(prato_paozinho)
 
 def main():
    restaurante_praca.exibir_cardapio
-   
+
 if __name__ == '__main__':
     main()
--- Restaurante-model/modelos/cardapio/item_cardapio.py
+++ Restaurante-model/modelos/cardapio/item_cardapio.py
@@ -1,4 +1,9 @@
-class ItemCardapio:
+from abc import ABC, abstractmethod
+
+class ItemCardapio():
     def __init__(self,nome,preco):
         self._nome= nome
         self._preco = preco
+    @abstractmethod
+    def aplicar_desconto(self):
+        pass
